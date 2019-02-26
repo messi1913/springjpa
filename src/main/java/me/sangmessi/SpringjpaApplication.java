abb87bc014ee3a5b88db36abc8e5133b8191df37
sangmessi <messi1913@gmail.com>
2019-02-26 21:11:30 +0900

    Spring JPA & REST Docs

---
+++ src/main/java/me/sangmessi/SpringjpaApplication.java
@@ -0,0 +1,13 @@
+package me.sangmessi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringjpaApplication.class, args);
+    }
+
+}
